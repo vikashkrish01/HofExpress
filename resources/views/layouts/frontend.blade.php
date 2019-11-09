12b7bec73f768d03a62daf7f064a94db08298541
travislykes <fopokuamoako@gmail.com>
2019-11-09 16:08:56 +0100

    home and about page additions

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="ie ie9"> <![endif]-->
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="pizza, delivery food, fast food, sushi, take away, chinese, italian food">
+    <meta name="description" content="">
+    <meta name="author" content="Ansonika">
+    <title>Hof Express - Quality delivery or take away food</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+    
+    <!-- GOOGLE WEB FONT -->
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic,300,300italic' rel='stylesheet' type='text/css'>
+
+    <!-- BASE CSS -->
+    <link href="{{ asset('css/base.css') }}" rel="stylesheet">
+    
+     <!-- SPECIFIC CSS -->
+     <link href="{{ asset('css/morphext.css') }}" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<!--[if lte IE 8]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a>.</p>
+<![endif]-->
+
+	<div id="preloader">
+        <div class="sk-spinner sk-spinner-wave" id="status">
+            <div class="sk-rect1"></div>
+            <div class="sk-rect2"></div>
+            <div class="sk-rect3"></div>
+            <div class="sk-rect4"></div>
+            <div class="sk-rect5"></div>
+        </div>
+    </div><!-- End Preload -->
+
+    <!-- Header ================================================== -->
+    <header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col--md-4 col-sm-4 col-xs-4">
+                <a href="index.html" id="logo">
+                <img src="img/logo.png" width="190" height="23" alt="" data-retina="true" class="hidden-xs">
+                <img src="img/logo_mobile.png" width="59" height="23" alt="" data-retina="true" class="hidden-lg hidden-md hidden-sm">
+                </a>
+            </div>
+            <nav class="col--md-8 col-sm-8 col-xs-8">
+            <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="javascript:void(0);"><span>Menu mobile</span></a>
+            <div class="main-menu">
+                <div id="header_menu">
+                    <img src="img/logo.png" width="190" height="23" alt="" data-retina="true">
+                </div>
+                <a href="#" class="open_close" id="close_in"><i class="icon_close"></i></a>
+                 <ul>
+                    <li class="submenu">
+                    <a href="javascript:void(0);" class="show-submenu">Home<i class="icon-down-open-mini"></i></a>
+                    <ul>
+                        <li><a href="index.html">Home Video background</a></li>
+                        <li><a href="index_2.html">Home Static image</a></li>
+                        <li><a href="index_3.html">Home Text rotator</a></li>
+                        <li><a href="index_8.html">Home Layer slider</a></li>
+                        <li><a href="index_4.html">Home Cookie bar</a></li>
+                        <li><a href="index_5.html">Home Popup</a></li>
+                        <li><a href="index_6.html">Home Mobile synthetic</a></li>
+                        <li><a href="index_7.html">Top Menu version 2</a></li>
+                    </ul>
+                    </li>
+                    <li class="submenu">
+                    <a href="javascript:void(0);" class="show-submenu">Restaurants<i class="icon-down-open-mini"></i></a>
+                    <ul>
+                        <li><a href="list_page.html">Row listing</a></li>
+                        <li><a href="grid_list.html">Grid listing</a></li>
+                        <li><a href="map_listing.html">Map listing</a></li>
+                        <li><a href="detail_page.html">Restaurant Menu</a></li>
+                        <li><a href="submit_restaurant.html">Submit Restaurant</a></li>
+                        <li><a href="cart.html">Order step 1</a></li>
+                        <li><a href="cart_2.html">Order step 2</a></li>
+                        <li><a href="cart_3.html">Order step 3</a></li>
+                        <li><a href="cart_datepicker.html">Order Date/Time picker</a></li>
+                    </ul>
+                    </li>
+                    <li><a href="about.html">About us</a></li>
+                    <li><a href="faq.html">Faq</a></li>
+                    <li class="submenu">
+                    <a href="javascript:void(0);" class="show-submenu">Pages<i class="icon-down-open-mini"></i></a>
+                    <ul>
+                        <li><a href="RTL_version/index.html">RTL version</a></li>
+                        <li><a href="admin.html">Admin section</a></li>
+                        <li><a href="submit_driver.html">Submit Driver</a></li>
+                        <li><a href="#0" data-toggle="modal" data-target="#login_2">User Login</a></li>
+                        <li><a href="#0" data-toggle="modal" data-target="#register">User Register</a></li>
+                        <li><a href="detail_page_2.html">Restaurant detail page</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="contacts.html">Contacts</a></li>
+                        <li><a href="coming_soon/index.html">Coming soon page</a></li>
+                        <li><a href="shortcodes.html">Shortcodes</a></li>
+                        <li><a href="icon_pack_1.html">Icon pack 1</a></li>
+                        <li><a href="icon_pack_2.html">Icon pack 2</a></li>
+                    </ul>
+                    </li>
+                    <li><a href="#0" data-toggle="modal" data-target="#login_2">Login</a></li>
+                    <li><a href="#0">Purchase this template</a></li>
+                </ul>
+            </div><!-- End main-menu -->
+            </nav>
+        </div><!-- End row -->
+    </div><!-- End container -->
+    </header>
+    <!-- End Header =============================================== -->
+    
+    <!-- SubHeader =============================================== -->
+   
+    @yield('content')
+
+    <!-- Footer ================================================== -->
+    <footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-3">
+                <h3>Secure payments with</h3>
+                <p>
+                    <img src="img/cards.png" alt="" class="img-responsive">
+                </p>
+            </div>
+            <div class="col-md-3 col-sm-3">
+                <h3>About</h3>
+                <ul>
+                    <li><a href="about.html">About us</a></li>
+                    <li><a href="faq.html">Faq</a></li>
+                    <li><a href="contacts.html">Contacts</a></li>
+                    <li><a href="#0" data-toggle="modal" data-target="#login_2">Login</a></li>
+                    <li><a href="#0" data-toggle="modal" data-target="#register">Register</a></li>
+                    <li><a href="#0">Terms and conditions</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-3" id="newsletter">
+                <h3>Newsletter</h3>
+                <p>
+                    Join our newsletter to keep be informed about offers and news.
+                </p>
+                <div id="message-newsletter_2">
+                </div>
+                <form method="post" action="assets/newsletter.php" name="newsletter_2" id="newsletter_2">
+                    <div class="form-group">
+                        <input name="email_newsletter_2" id="email_newsletter_2" type="email" value="" placeholder="Your mail" class="form-control">
+                    </div>
+                    <input type="submit" value="Subscribe" class="btn_1" id="submit-newsletter_2">
+                </form>
+            </div>
+            <div class="col-md-2 col-sm-3">
+                <h3>Settings</h3>
+                <div class="styled-select">
+                    <select class="form-control" name="lang" id="lang">
+                        <option value="English" selected>English</option>
+                        <option value="French">French</option>
+                        <option value="Spanish">Spanish</option>
+                        <option value="Russian">Russian</option>
+                    </select>
+                </div>
+                <div class="styled-select">
+                    <select class="form-control" name="currency" id="currency">
+                        <option value="USD" selected>USD</option>
+                        <option value="EUR">EUR</option>
+                        <option value="GBP">GBP</option>
+                        <option value="RUB">RUB</option>
+                    </select>
+                </div>
+            </div>
+        </div><!-- End row -->
+        <div class="row">
+            <div class="col-md-12">
+                <div id="social_footer">
+                    <ul>
+                        <li><a href="#0"><i class="icon-facebook"></i></a></li>
+                        <li><a href="#0"><i class="icon-twitter"></i></a></li>
+                        <li><a href="#0"><i class="icon-google"></i></a></li>
+                        <li><a href="#0"><i class="icon-instagram"></i></a></li>
+                        <li><a href="#0"><i class="icon-pinterest"></i></a></li>
+                        <li><a href="#0"><i class="icon-vimeo"></i></a></li>
+                        <li><a href="#0"><i class="icon-youtube-play"></i></a></li>
+                    </ul>
+                    <p>
+                        © Quick Food 2015
+                    </p>
+                </div>
+            </div>
+        </div><!-- End row -->
+    </div><!-- End container -->
+    </footer>
+    <!-- End Footer =============================================== -->
+
+<div class="layer"></div><!-- Mobile menu overlay mask -->
+
+<!-- Login modal -->   
+<div class="modal fade" id="login_2" tabindex="-1" role="dialog" aria-labelledby="myLogin" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content modal-popup">
+				<a href="#" class="close-link"><i class="icon_close_alt2"></i></a>
+				<form action="#" class="popup-form" id="myLogin">
+                	<div class="login_icon"><i class="icon_lock_alt"></i></div>
+					<input type="text" class="form-control form-white" placeholder="Username">
+					<input type="text" class="form-control form-white" placeholder="Password">
+					<div class="text-left">
+						<a href="#">Forgot Password?</a>
+					</div>
+					<button type="submit" class="btn btn-submit">Submit</button>
+				</form>
+			</div>
+		</div>
+	</div><!-- End modal -->   
+    
+<!-- Register modal -->   
+<div class="modal fade" id="register" tabindex="-1" role="dialog" aria-labelledby="myRegister" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content modal-popup">
+				<a href="#" class="close-link"><i class="icon_close_alt2"></i></a>
+				<form action="#" class="popup-form" id="myRegister">
+                	<div class="login_icon"><i class="icon_lock_alt"></i></div>
+					<input type="text" class="form-control form-white" placeholder="Name">
+					<input type="text" class="form-control form-white" placeholder="Last Name">
+                    <input type="email" class="form-control form-white" placeholder="Email">
+                    <input type="text" class="form-control form-white" placeholder="Password"  id="password1">
+                    <input type="text" class="form-control form-white" placeholder="Confirm password"  id="password2">
+                    <div id="pass-info" class="clearfix"></div>
+					<div class="checkbox-holder text-left">
+						<div class="checkbox">
+							<input type="checkbox" value="accept_2" id="check_2" name="check_2" />
+							<label for="check_2"><span>I Agree to the <strong>Terms &amp; Conditions</strong></span></label>
+						</div>
+					</div>
+					<button type="submit" class="btn btn-submit">Register</button>
+				</form>
+			</div>
+		</div>
+	</div><!-- End Register modal -->
+    
+<!-- COMMON SCRIPTS -->
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/common_scripts_min.js"></script>
+<script src="js/functions.js"></script>
+<script src="assets/validate.js"></script>
+
+<!-- SPECIFIC SCRIPTS -->
+<script src="js/morphext.min.js"></script>
+<script>
+$("#js-rotating").Morphext({
+    animation: "fadeIn", // Overrides default "bounceIn"
+    separator: ",", // Overrides default ","
+    speed: 2300, // Overrides default 2000
+    complete: function () {
+        // Overrides default empty function
+    }
+});
+</script>
+
+</body>
+</html>
